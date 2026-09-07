a2037924e3467e40f21416ed86139e9c3cdac767
kairat-genius <k_kurmanov@list.ru>
2026-09-08 01:37:45 +0600

    brand api

--- frontend/src/shared/context/catalog-data/api/getCategoryList.ts
+++ frontend/src/shared/context/catalog-data/api/getCategoryList.ts
@@ -1,17 +1,18 @@
 import { CATEGORY_TREE } from "@/shared/api/endpoints";
 import { CategoryListResponseType } from "@/types/category-list.type";
-
+import CategoryData from "./categoryData.json";
 export async function getCategoryList(): Promise<CategoryListResponseType> {
-  const res = await fetch(CATEGORY_TREE, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  // const res = await fetch(CATEGORY_TREE, {
+  //   method: "GET",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  // });
 
-  if (!res.ok) {
-    throw new Error(`HTTP Error: ${res.status}`);
-  }
+  // if (!res.ok) {
+  //   throw new Error(`HTTP Error: ${res.status}`);
+  // }
 
-  return res.json();
+  // return res.json();
+  return CategoryData;
 }
